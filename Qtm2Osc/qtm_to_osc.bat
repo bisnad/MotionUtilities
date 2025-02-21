ae8420b9239297b32ba20edd4c625f883ff1b528
bisnad <daniel.bisig@zhdk.ch>
2025-02-21 17:30:57 +0100

    fixed a mistake in qtm installation script

--- Qtm2Osc/qtm_to_osc.bat
+++ Qtm2Osc/qtm_to_osc.bat
@@ -1,8 +1,8 @@
 set CONDA_PATH=C:\Users\%USERNAME%\anaconda3
-set ENV_NAME=premiere
-set PYTHON_VERSION=3.10
+set ENV_NAME=qtm
+set PYTHON_VERSION=3.8
 
 call %CONDA_PATH%\Scripts\activate.bat
 call conda activate %ENV_NAME%
-python mocap_player.py
+python qtm_to_osc.py
 pause
