638d8344506320f6efa7acfc87106dadb72dfe37
bisnad <daniel.bisig@zhdk.ch>
2025-06-04 12:47:44 +0200

    added a new version of the MocapPlayer that doesn't require resampled FBX files

---
+++ MocapPlayer_v2/mocap_player.py
@@ -0,0 +1,105 @@
+
+import os, sys, time, subprocess
+
+import motion_player 
+import motion_sender
+import motion_control
+import motion_gui
+
+"""
+Setup Motion Player
+"""
+
+"""
+motion_player.config = { 
+    "file_name": "C:/Users/dbisig/Desktop/Canal_14-08-2023-001_Muriel_Fluid_1.fbx",
+    "fps": 50
+    }
+"""
+
+"""
+motion_player.config = { 
+    "file_name": "C:/Users/dbisig/Desktop/Daniel_Untitled_Take1_15.12.fbx",
+    "fps": 50
+    }
+"""
+
+motion_player.config = { 
+    "file_name": "data/mocap/Muriel_Take1.fbx",
+    "fps": 50
+    }
+
+"""
+motion_player.config = { 
+    "file_name": "../../../Data/Mocap/Zed/Daniel/Solos/fbx_30hz/daniel_zed_solo1.fbx",
+    "fps": 30
+    }
+"""
+
+"""
+motion_player.config = { 
+    "file_name": "../../../Data/Mocap/Qualisys/Stocos/Solos/fbx_50hz/polytopia_fullbody_take1.fbx",
+    "fps": 50
+    }
+"""
+
+"""
+motion_player.config = { 
+    "file_name": "../../../Data/Mocap/Xsens/Stocos/Solos/fbx_50hz/Muriel_Embodied_Machine_variation.fbx",
+    "fps": 50
+    }
+"""
+
+player = motion_player.MotionPlayer(motion_player.config)
+
+
+"""
+OSC Sender
+"""
+
+motion_sender.config["ip"] = "127.0.0.1"
+motion_sender.config["port"] = 9007
+
+osc_sender = motion_sender.OscSender(motion_sender.config)
+
+"""
+Setup Motion GUI
+"""
+
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt
+import pyqtgraph as pg
+import pyqtgraph.opengl as gl
+from pathlib import Path
+
+motion_gui.config["player"] = player
+motion_gui.config["sender"] = osc_sender
+
+app = QtWidgets.QApplication(sys.argv)
+gui = motion_gui.MotionGui(motion_gui.config)
+
+# set close event
+def closeEvent():
+    QtWidgets.QApplication.quit()
+app.lastWindowClosed.connect(closeEvent) # myExitHandler is a callable
+
+"""
+OSC Control
+"""
+
+motion_control.config["gui"] = gui
+motion_control.config["ip"] = "0.0.0.0"
+motion_control.config["port"] = 9002
+
+osc_control = motion_control.MotionControl(motion_control.config)
+
+
+"""
+Start Application
+"""
+
+osc_control.start()
+gui.show()
+app.exec_()
+
+osc_control.stop()
