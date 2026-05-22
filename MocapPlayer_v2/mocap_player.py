7fb7ca637adfd88c6364f9ff43fa453c87618c8c
bisnad <daniel.bisig@zhdk.ch>
2026-05-22 11:39:57 +0200

    added some new screenshots

--- MocapPlayer_v2/mocap_player.py
+++ MocapPlayer_v2/mocap_player.py
@@ -10,12 +10,12 @@ import motion_gui
 Setup Motion Player
 """
 
-
+"""
 motion_player.config = { 
     "file_name": "data/mocap/Take_3_50fps_crop.fbx",
     "fps": 50
     }
-
+"""
 
 """
 motion_player.config = { 
@@ -24,12 +24,12 @@ motion_player.config = {
     }
 """
 
-"""
+
 motion_player.config = { 
     "file_name": "../../../Data/Mocap/Zed/Daniel/Solos/fbx_30hz/daniel_zed_solo2.fbx",
     "fps": 30
     }
-"""
+
 
 """
 motion_player.config = { 
